5b6f3b5cc2feb923a263e7b339fb74e89f8da5c0
Khaled Almahallawy <almahallawy@gmail.com>
2018-09-20 19:31:26 -0700

    fix eqlist?

--- scheme/14-let-there-be-names.rkt
+++ scheme/14-let-there-be-names.rkt
@@ -51,21 +51,22 @@
 (leftmost-3 '(((aac) ()) () (e)))
 
 ;;============================================
-(define  eqlist?
+
+(define eqlist?
   (lambda (l1 l2)
     (cond
       ((and (null? l1) (null? l2)) #t)
-      ((and (null? l1) (atom? (car l2))) #f)
-      ((null? l1) #f)
+      ((or (null? l1) (null? l2)) #f)
       ((and (atom? (car l1)) (atom? (car l2)))
        (and (eq? (car l1) (car l2))
             (eqlist? (cdr l1) (cdr l2))))
-      ((atom? (car l1)) #f)
-      ((null? l2) #f)
-      ((atom? (car l2)) #f)
-      (else (and (eqlist? (car l1) (car l2))
-                 (eqlist? (cdr l1) (cdr l2)))))))
+      ((or (atom? (car l1)) (atom? (car l2))) #f)
+      (else
+       (and (eqlist? (car l1) (car l2))
+            (eqlist? (cdr l1) (cdr l2)))))))
 
+(eqlist? '(1) '())
+(eqlist? '() '(1))
 (eqlist? '(strawberry ice cream) '(strawberry ice cream))
 (eqlist? '(strawberry ice cream) '(strawberry cream ice))
 (eqlist? '(banana ((split))) '((banana) (split)))
