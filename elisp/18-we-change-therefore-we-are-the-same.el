175e37648bfa3a6a6e0c4e040deb5e17cd2b3181
Khaled Almahallawy <almahallawy@gmail.com>
2019-03-06 13:38:19 -0800

    ch18 elisp wip

--- elisp/18-we-change-therefore-we-are-the-same.el
+++ elisp/18-we-change-therefore-we-are-the-same.el
@@ -9,9 +9,8 @@
 (defun lots (m)
   (cond
    ((zerop m) (quote ()))
-   (t (kons (quote eggo)
+   (t (kons (quote egg)
 	    (lots (1- m))))))
-
 
 (lots 3)
 (lots 5)
@@ -25,22 +24,54 @@
 (lenkth (lots 5))
 (lenkth (lots 12))
 
+;;Create a list of four egges from (lots 3)
+(kons (quote egg) (lots 3))
+
 
 (setq kounter ())
-(setq set-counter ())
+(setq set-kounter ())
 
-(setq konC
+(setq konsC
       (let ((N 0))
 	(setq kounter
 	      (lambda ()
 		N))
-	(setq set-counter
+	(setq set-kounter
 	      (lambda (x)
 		(setq N x)))
 	(lambda (x y)
 	  (setq N (1+ N))
 	  (kons x y))))
 
+(defun add-at-end (l)
+  (cond
+   ((null (kdr l))
+    (funcall konsC (kar l)
+	     (kons (quote eggo)
+		   (quote ()))))
+   (t (funcall konsC (kar l)
+	    (add-at-end (kdr l))))))
+
+(funcall set-kounter 0)
+(add-at-end (lots 3))
+(funcall kounter)
+
+
+(defun add-at-end-too (l)
+  (letrec
+      ((A (lambda (ls)
+	    (cond
+	     ((null (kdr ls))
+	      (rplacd ls
+		      (kons (quote eggo)
+			    (quote ()))))
+	     (t (funcall A (kdr ls)))))))
+    (funcall A l)
+    l))
+
+(funcall set-kounter 0)
+(add-at-end-too (lots 3))
+(funcall kounter)
 
 
 
@@ -48,3 +79,19 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
